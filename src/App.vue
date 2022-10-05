5d583ecc711ad3b0bb7849133bce8cb28a83427f
cazser <2264712072@qq.com>
2022-10-06 06:51:53 +0800

    +1

--- src/App.vue
+++ src/App.vue
@@ -5,26 +5,56 @@ import HelloWorld from './components/HelloWorld.vue'
 import wordList from './WordList';
 import Button from './components/Button.vue';
 import Dialog from './components/Dialog.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 const y = ref(false);
 const toggle= ()=>{
   y.value = !y.value;
 }
+const inputRef= ref(null);
+
 const curIndex = ref(0);
 const onSubmit=(e)=>{
   e.preventDefault();
+  
+  if(inputRef.value.value === wordList[curIndex.value].日文){
+    const synUtterance = new window.SpeechSynthesisUtterance();
+    synUtterance.text = inputRef.value.value;
+    synUtterance.lang = "ja-JP";
+    synUtterance.pitch = 1; //音调高低0～2，默认1
+    synUtterance.rate = 1;
+    window.speechSynthesis.speak(synUtterance);
+    next();
+  }else{
+    const synUtterance = new window.SpeechSynthesisUtterance();
+    synUtterance.text = "いいえ";
+    synUtterance.lang = "ja-JP";
+    synUtterance.pitch = 1; //音调高低0～2，默认1
+    synUtterance.rate = 1;
+    window.speechSynthesis.speak(synUtterance);
+  }
+         
 }
 const onHelp=(e)=>{
   e.preventDefault();
+  const synUtterance = new window.SpeechSynthesisUtterance();
+    synUtterance.text = wordList[curIndex.value].日文;
+    synUtterance.lang = "ja-JP";
+    synUtterance.pitch = 1; //音调高低0～2，默认1
+    synUtterance.rate = 1;
+    window.speechSynthesis.speak(synUtterance);
 }
 const next = ()=>{
   if(curIndex.value<wordList.length){
+    inputRef.value.value="";
     curIndex.value++;
+  }else{
+    y.value = false;
   }
 }
 const end=()=>{
   y.value = false;
 }
+
 </script>
 
 <template>
@@ -35,13 +65,14 @@ const end=()=>{
   <Dialog v-model:visible="y">
     <template #default>
             <div>
-              写出中文对应的日文,
+              写出中文对应的日文
               <form>
-                {{wordList[curIndex].中文}}<input />
-                <Button @click="onSubmit">
+                {{wordList[curIndex].中文}}:<input ref="inputRef"
+                class="input" placeholder="填写对应的日文"/>
+                <Button :small="true" @click="onSubmit">
                   提交
                 </Button>
-                <Button @click="onHelp">
+                <Button :small="true" @click="onHelp">
                   帮助
                 </Button>
               </form>
@@ -73,5 +104,11 @@ const end=()=>{
 .wordListCell{
   min-width: 200px;
   display: inline-block;
+}
+
+.input{
+  padding: 4px;
+  margin: 8px;
+
 }
 </style>
